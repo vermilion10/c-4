d470ddfcc6216c3c890a2cca722cdaeccd24473a
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-01-16 06:12:28 +0700

    pola 3

--- pola3.c
+++ pola3.c
@@ -1,1 +1,30 @@
-desc: 
+// desc: program untuk menampikan pola
+// contoh input: 5
+// contoh output: 1 2 3 4 5
+//                6 7 8 9
+//                10 11 12
+//                13 14
+//                15
+// author: vermilion10
+// date: 16 januari 2025
+
+#include <stdio.h>
+
+//fungsi rekursif untuk mencetak pola
+void printPola(int n, int i, int j, int k){
+    if (i == n) return;
+    if (j == n - i){
+        printf("\n");
+        printPola(n, i + 1, 0, k);
+        return;
+    }
+    printf("%d ", k++);
+    printPola(n, i, j + 1, k);
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printPola(n, 0, 0, 1);
+    return 0;
+}
